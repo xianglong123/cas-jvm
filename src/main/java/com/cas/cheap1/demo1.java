555269a6da7410b72f2e4c4632cab048076db29b
xianglong <1391086179@qq.com>
2021-02-03 18:02:33 +0800

    Initial commit

---
+++ src/main/java/com/cas/cheap1/demo1.java
@@ -0,0 +1,17 @@
+package com.cas.cheap1;
+
+/**
+ * @author: xianglong[1391086179@qq.com]
+ * @date: 下午5:52 2021/2/3
+ * @version: V1.0
+ * @review:
+ */
+public class demo1 {
+
+    public static void main(String[] args) {
+        int i =2;
+        int j =3;
+        int k = i + j;
+    }
+
+}
